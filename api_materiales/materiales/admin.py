e8dd593ce777adc81c082261337b19328e398b90
federmozzon <mozzonfederico@gmail.com>
2022-10-17 19:31:01 -0300

    Consulto reserva de lugares

--- api_materiales/materiales/admin.py
+++ api_materiales/materiales/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Material, Material_Provider, Material_arrived, Provider
+from .models import Factory_Place, Material, Material_Provider, Material_arrived, Provider, Reserve_Factory
 
 # Register your models here.
 admin.site.register(Provider)
 admin.site.register(Material)
+admin.site.register(Reserve_Factory)
+admin.site.register(Factory_Place)
 admin.site.register(Material_Provider)
 admin.site.register(Material_arrived)
